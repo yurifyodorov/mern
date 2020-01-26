78cb020c14ddb3d2213974d75aa789a654693d58
Yury <yuri.inpost@yandex.ru>
2020-01-26 16:18:22 +0300

    Create Update Profile Routes completed

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,75 +12,75 @@ const User = require('../../models/User');
 // @desc    Register user
 // @access  Public
 router.post(
-    '/',
-    [
-        check('name', 'Name is required')
-            .not()
-            .isEmpty(),
-        check('email', 'Please include a valid email').isEmail(),
-        check(
-            'password',
-            'Please enter a password with 6 or more cheracters'
-        ).isLength({ min: 6 })
-    ],
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-
-        const { name, email, password } = req.body;
-
-        try {
-            // See if user exist
-            let user = await User.findOne({ email });
-
-            if (user) {
-                return res
-                    .status(400)
-                    .json({ errors: [{ msg: 'User already exist' }] });
-            }
-
-            // Get users gravatar (011 User Registration, 07:07)
-            const avatar = gravatar.url(email, {
-                s: '200',
-                r: 'pg',
-                d: 'mm'
-            });
-
-            user = new User({
-                name,
-                email,
-                avatar,
-                password // need to encrypt
-            });
-
-            // Encrypt password (011 User Registration, 09:04)
-            const salt = await bcrypt.genSalt(10);
-            user.password = await bcrypt.hash(password, salt);
-            await user.save();
-
-            // Return jsonwebtoken (012 Implementing JWT, 02:37)
-            const payload = {
-                user: {
-                    id: user.id
-                }
-            };
-
-            jwt.sign(
-                payload,
-                config.get('jwtSecret'),
-                { expiresIn: 360000 }, // 360000 seconds (Recommended)
-                (err, token) => {
-                    if (err) throw err;
-                    res.json({ token });
-                }
-            );
-        } catch (err) {
-            console.error(err.massage);
-            res.status(500).send('Server error');
-        }
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more cheracters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exist
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exist' }] });
+      }
+
+      // Get users gravatar (011 User Registration, 07:07)
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password // need to encrypt
+      });
+
+      // Encrypt password (011 User Registration, 09:04)
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      // Return jsonwebtoken (012 Implementing JWT, 02:37)
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 }, // 360000 seconds (Recommended)
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.massage);
+      res.status(500).send('Server error');
+    }
+  }
 );
 
 module.exports = router;
