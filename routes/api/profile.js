7f14f1e31cbc65928654e779881aa278ea6fa1e9
Yury Fyodorov <yuri.inpost@yandex.ru>
2020-01-27 10:39:41 +0300

    Delete Profile Experience completed

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -211,8 +211,11 @@ router.put(
 
     try {
       const profile = await Profile.findOne({ user: req.user.id });
+
       profile.experience.unshift(newExp);
+
       await profile.save();
+
       res.json(profile);
     } catch (err) {
       console.error(err.message);
@@ -220,5 +223,28 @@ router.put(
     }
   }
 );
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
