0080ee5de683cc68336989849d911d1c340347a3
Yury <yuri.inpost@yandex.ru>
2020-01-31 23:56:18 +0300

    fix mistake

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -34,7 +34,7 @@ const Dashboard = ({
 
           <div className='my-2'>
             <button className='btn btn-danger' onClick={() => deleteAccount()}>
-              <i className='fas fa-user-minus'></i> Delete My Accoun
+              <i className='fas fa-user-minus'></i> Delete My Account
             </button>
           </div>
         </Fragment>
