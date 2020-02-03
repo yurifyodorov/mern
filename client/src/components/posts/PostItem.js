77fffdc3f11df9d69ca3f679eed1dc618522250f
Yury Fyodorov <yuri.inpost@yandex.ru>
2020-02-03 15:29:14 +0300

    Like  Unlike Functionality completed

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,29 +3,40 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
+import { addLike, removeLike } from '../../actions/post';
 
 const PostItem = ({
+  addLike,
+  removeLike,
   auth,
   post: { _id, text, name, avatar, user, likes, comments, date }
 }) => (
   <div className='post bg-white p-1 my-1'>
     <div>
-      <a href='profile.html'>
+      <Link to={`/profile/${user}`}>
         <img className='round-img' src={avatar} alt='' />
         <h4>{name}</h4>
-      </a>
+      </Link>
     </div>
     <div>
       <p className='my-1'>{text}</p>
       <p className='post-date'>
         Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
       </p>
-      <button type='button' className='btn btn-light'>
+      <button
+        onClick={() => addLike(_id)}
+        type='button'
+        className='btn btn-light'
+      >
         <i className='fas fa-thumbs-up' />{' '}
         <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
       </button>
-      <button type='button' className='btn btn-light'>
-        <i className='fas fa-thumbs-down' />
+      <button
+        onClick={() => removeLike(_id)}
+        type='button'
+        className='btn btn-light'
+      >
+        <i className='fas fa-thumbs-down' />{' '}
       </button>
       <Link to={`/post/${_id}`} className='btn btn-primary'>
         Discussion{' '}
@@ -51,4 +62,4 @@ const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps, {})(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
