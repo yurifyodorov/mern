632d9b3083fde86da101fadd29cff9dc5ca03ef0
Yury Fyodorov <yuri.inpost@yandex.ru>
2020-02-04 15:21:47 +0300

    delete one profile from Atlas

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -13,7 +13,7 @@ const ProfileItem = ({
 }) => {
   return (
     <div className='profile bg-light'>
-      <img src={avatar} className='round-img' alt='avatar' />
+      <img src={avatar} alt='avatar' className='round-img' />
       <div>
         <h2>{name}</h2>
         <p>
